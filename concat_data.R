b27ec872c4bc36730ac4d0639480dc07b67db9fd
haututu <35088807+haututu@users.noreply.github.com>
2020-06-29 17:54:24 +1200

    Created data for shiny app.

---
+++ concat_data.R
@@ -0,0 +1,25 @@
+# Concatenates the four input CSVs into a single dataset
+
+library(tidyverse)
+
+bind_rows(
+  read_csv("1. ALL cannabis.csv") %>%
+    mutate(topic = "cannabis",
+           retweets = TRUE,
+           Date = as.Date(Date, format = "%m/%d/%Y")),
+  
+  read_csv("2. Cannabis - Unique.csv") %>%
+    mutate(topic = "cannabis",
+           retweets = FALSE,
+           Date = as.Date(Date, format = "%m/%d/%Y")),
+
+  read_csv("3. ALL Referendum.csv") %>%
+    mutate(topic = "referendum",
+           retweets = TRUE,
+           Date = as.Date(Date, format = "%m/%d/%Y")),
+
+  read_csv("4. Unique Referendum.csv") %>%
+    mutate(topic = "referendum",
+           retweets = FALSE)
+  ) %>%
+  gather(valence, count, 2:4)
